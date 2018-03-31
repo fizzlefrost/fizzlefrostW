4f9c8462b15d0e1dce66b46607f949eb364b0b88
fizzlefrost <englingurru@gmail.com>
2018-03-31 11:45:17 +0900

    test

---
+++ Assets/Enemy.cs
@@ -0,0 +1,329 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float enemyspeed;
+    private bool flipped = false;
+    private Rigidbody2D en2d;
+    private Animator animatore;
+    private bool isMoving = false;
+    private bool Up = false;
+    private bool Down = false;
+    private bool Left = false;
+    private bool Right = false;
+    private bool IsAttacking = false;
+    private bool attac = false;
+    public Transform player;
+    public float distanceX, distanceY, distance;
+    float movingTime = 5f;
+    public float chargeTime;
+    float startcharge;
+    AudioSource SoundKrikEnemy1;
+
+    void Start () {
+        en2d = GetComponent<Rigidbody2D>();
+        animatore = GetComponent<Animator>();
+        SoundKrikEnemy1 = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate () {
+            
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isMoving = true;
+            distanceX = player.position.x - transform.position.x;
+            distanceY = player.position.y - transform.position.y;
+            distance = Vector2.Distance(player.position, transform.position);
+            if (distance > 3.5)
+            {
+                if (distanceX > 0)
+                {
+                    if (distanceY > 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y + enemyspeed);
+                        Up = true;
+                        Down = false;
+                        Right = true;
+                        Left = false;
+                        isMoving = true;
+                    }
+                    if (distanceY < 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y - enemyspeed);
+                        Up = false;
+                        Down = true;
+                        Right = true;
+                        Left = false;
+                        isMoving = true;
+                    }
+                    if (distanceY == 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y);
+                        Up = true;
+                        Down = false;
+                        Right = true;
+                        Left = false;
+                        isMoving = true;
+                    }
+                }
+                if (distanceX < 0)
+                {
+                    if (distanceY > 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y + enemyspeed);
+                        Up = true;
+                        Down = false;
+                        Right = false;
+                        Left = true;
+                        isMoving = true;
+                    }
+                    if (distanceY < 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y - enemyspeed);
+                        Up = false;
+                        Down = true;
+                        Right = false;
+                        Left = true;
+                        isMoving = true;
+                    }
+                    if (distanceY == 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y);
+                        Up = false;
+                        Down = true;
+                        Right = false;
+                        Left = true;
+                        isMoving = true;
+                    }
+                }
+                if (distanceX == 0)
+                {
+                    if (distanceY > 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x, this.transform.position.y + enemyspeed);
+                        Up = true;
+                        Down = false;
+                        Right = true;
+                        Left = false;
+                        isMoving = true;
+                    }
+                    if (distanceY < 0)
+                    {
+                        transform.position = new Vector2(this.transform.position.x, this.transform.position.y - enemyspeed);
+                        Up = false;
+                        Down = true;
+                        Right = true;
+                        Left = false;
+                        isMoving = true;
+                    }
+                }
+                startcharge = 0;
+            }
+            else
+            {
+                startcharge += 0.1f;
+            }
+            
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            isMoving = true;
+            distanceX = player.position.x - transform.position.x;
+            distanceY = player.position.y - transform.position.y;
+            distance = Vector2.Distance(player.position, transform.position);
+            if (!IsAttacking)
+            {
+                if (distance > 3.5)
+                {
+                    if (distanceX > 0)
+                    {
+                        if (distanceY > 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y + enemyspeed);
+                            Up = true;
+                            Down = false;
+                            Right = true;
+                            Left = false;
+                            isMoving = true;
+                        }
+                        if (distanceY < 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y - enemyspeed);
+                            Up = false;
+                            Down = true;
+                            Right = true;
+                            Left = false;
+                            isMoving = true;
+                        }
+                        if (distanceY == 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y);
+                            Up = true;
+                            Down = false;
+                            Right = true;
+                            Left = false;
+                            isMoving = true;
+                        }
+                    }
+                    if (distanceX < 0)
+                    {
+                        if (distanceY > 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y + enemyspeed);
+                            Up = true;
+                            Down = false;
+                            Right = false;
+                            Left = true;
+                            isMoving = true;
+                        }
+                        if (distanceY < 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y - enemyspeed);
+                            Up = false;
+                            Down = true;
+                            Right = false;
+                            Left = true;
+                            isMoving = true;
+                        }
+                        if (distanceY == 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y);
+                            Up = false;
+                            Down = true;
+                            Right = false;
+                            Left = true;
+                            isMoving = true;
+                        }
+                    }
+                    if (distanceX == 0)
+                    {
+                        if (distanceY > 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x, this.transform.position.y + enemyspeed);
+                            Up = true;
+                            Down = false;
+                            Right = true;
+                            Left = false;
+                            isMoving = true;
+                        }
+                        if (distanceY < 0)
+                        {
+                            transform.position = new Vector2(this.transform.position.x, this.transform.position.y - enemyspeed);
+                            Up = false;
+                            Down = true;
+                            Right = true;
+                            Left = false;
+                            isMoving = true;
+                        }
+                    }
+                    startcharge = 0;
+                }
+                else
+                {
+                    IsAttacking = true;
+                    isMoving = false;
+                    Up = false;
+                    Down = false;
+                    Right = false;
+                    Left = false;
+                    startcharge += 0.01f;
+                    if (distanceX > 0 && flipped)
+                    {
+                        Flip();
+                        flipped = !flipped;
+                    }
+                    if (distanceX < 0 && !flipped)
+                    {
+                        Flip();
+                        flipped = !flipped;
+                    }
+                }
+            }
+            else
+            {
+                if (startcharge >= chargeTime)
+                {
+                    
+                    SoundKrikEnemy1.Play();
+                    attac = true;
+                    IsAttacking = false;
+                    startcharge = 0;
+                    if (distanceX > 0)
+                    {
+                        player.position = new Vector2(player.position.x + 1, player.position.y);
+                        player.Find("backgsrnd1").transform.position = new Vector3(player.Find("backgsrnd1").transform.position.x, player.Find("backgsrnd1").transform.position.y, (player.Find("backgsrnd1").transform.position.z + 1) * 2);
+                    }
+                    if (distanceX < 0)
+                    {
+                        player.position = new Vector2(player.position.x - 1, player.position.y);
+                        player.Find("backgsrnd1").transform.position = new Vector3(player.Find("backgsrnd1").transform.position.x, player.Find("backgsrnd1").transform.position.y, (player.Find("backgsrnd1").transform.position.z + 1) * 2);
+                    }
+                }
+                else {
+                    if (distance > 3.5)
+                    {
+                        startcharge = 0;
+                    }
+                    else startcharge += 0.01f;
+                }
+                Up = false;
+                Down = false;
+                Right = false;
+                Left = false;
+                isMoving = false;
+            }
+            animatore.SetFloat("distance", Mathf.Abs(distance));
+            animatore.SetBool("Attack", IsAttacking);
+            animatore.SetFloat("charge", startcharge);
+            animatore.SetBool("up", Up);
+            animatore.SetBool("down", Down);
+            animatore.SetBool("right", Right);
+            animatore.SetBool("left", Left);
+            animatore.SetBool("isMoving", isMoving);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Up = false;
+        Down = false;
+        Right = false;
+        Left = false;
+        isMoving = false;
+        IsAttacking = false;
+        attac = false;
+        startcharge = 0;
+    }
+    private void MoveAnimate(float Hor, float Ver)
+    {
+        distanceX = player.position.x - transform.position.x;
+        distanceY = player.position.y - transform.position.y;
+        if (distanceX > 0)
+        {
+            if (distanceY > 0)
+                transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y + enemyspeed);
+            else 
+                transform.position = new Vector2(this.transform.position.x + enemyspeed, this.transform.position.y - enemyspeed);
+        }
+        if (distanceX < 0)
+        {
+            if (distanceY > 0)
+                transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y + enemyspeed);
+            else
+                transform.position = new Vector2(this.transform.position.x - enemyspeed, this.transform.position.y - enemyspeed);
+        }
+    }
+    private void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public GameObject meme;
+    public float speed;
+    private bool flipped;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    public Transform player;
+    private bool yes = false;
+    private bool right = false;
+    private bool left = false;
+    private bool up = false;
+    private bool down = true;
+    private bool stand = false;
+    private bool isDead = false;
+    private AudioSource SoundPlayerSteps;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        SoundPlayerSteps = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        MoveAnimate(moveHorizontal, moveVertical);
+
+    }
+    private void MoveAnimate(float moveHorizontal, float moveVertical)
+    {
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        animator.SetFloat("speedX", Mathf.Abs(moveHorizontal));
+        animator.SetFloat("speedY", Mathf.Abs(moveVertical));
+        if (moveHorizontal > 0)
+            right = true;
+        else right = false;
+        if (moveHorizontal < 0)
+            left = true;
+        else left = false;
+        if ((moveVertical > 0) && (moveHorizontal == 0))
+            up = true;
+        else up = false;
+        if ((moveVertical == 0) && (moveHorizontal == 0))
+            stand = true;
+        else stand = false;
+        if ((moveVertical < 0) && (moveHorizontal == 0))
+            down = true;
+        else down = false;
+        if ((right == true) || (left == true) || (up == true) || (down == true))
+        {
+            if (SoundPlayerSteps.isPlaying == false)
+                SoundPlayerSteps.Play();
+        }
+        animator.SetBool("up", up);
+        animator.SetBool("down", down);
+        animator.SetBool("right", right);
+        animator.SetBool("left", left);
+        animator.SetBool("stand", stand);
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            if (Input.GetKey("q"))
+                other.gameObject.SetActive(false);
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            if (Input.GetKey("q"))
+                other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("PickUpLetter"))
+        {
+            if (Input.GetKey("e"))
+                other.gameObject.SetActive(false);
+        }
+    }
+    void Death()
+    {
+        if (player.Find("backgsrnd1").transform.position.z >= 100)
+        {
+            isDead = true;
+        }
+    }
+}
